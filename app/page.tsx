f4265ed41b053b7e24b4313fe2cf6ee71411d94d
Jhonesm13 <jhone.menezes@edu.pucrs.br>
2025-08-22 15:45:06 -0300

    add booking item on home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,10 @@ import Header from "./_components/header";
 import { Input } from "./_components/ui/input";
 import { Button } from "./_components/ui/button";
 import Image from "next/image";
+import { Card, CardContent } from "./_components/ui/card";
+import { Badge } from "./_components/ui/badge";
+import { Avatar } from "./_components/ui/avatar";
+import { AvatarImage } from "@radix-ui/react-avatar";
 
 export default function Home() {
   return (
@@ -24,6 +28,28 @@ export default function Home() {
           <Image alt="Agende nos melhores" 
           src="/banner-01.png" fill className="object-cover rounded-xl"/>
         </div>
+
+        <Card className="mt-6">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">
+                Confirmado
+              </Badge>
+              <h3 className="font-semibold">Corte de cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sn">Vintage Barber</p>
+              </div>
+            </div>
+            <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
+              <p className="text-sm">Agosto</p>
+              <p className="text-xl">22</p>
+              <p className="text-sm">15:99</p>
+            </div>
+          </CardContent>
+        </Card>
 
       </div>
 
