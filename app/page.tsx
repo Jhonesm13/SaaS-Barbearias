c499d1579ac6ac8ca4ff18202993f275200d2107
Jhonesm13 <jhone.menezes@edu.pucrs.br>
2025-08-23 04:03:39 -0300

    add quick search and BookingItemComponent

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,19 @@ import { Input } from "./_components/ui/input";
 import { Button } from "./_components/ui/button";
 import Image from "next/image";
 import { Card, CardContent } from "./_components/ui/card";
-import { Badge } from "./_components/ui/badge";
-import { Avatar } from "./_components/ui/avatar";
-import { AvatarImage } from "@radix-ui/react-avatar";
 import { db } from "./_lib/prisma";
 import BarbershopItem from "./_components/barbershop-item";
+import { quickSearchOptions } from "./_constants/search";
+import BookingItem from "./_components/booking-item";
 
 const Home = async () => {
 
   const barbershops = await db.barbershop.findMany({})
+  const popularBarbershops = await db.barbershop.findMany({
+    orderBy: {
+      name: "desc"
+    }
+  })
 
   return (
     <div>
@@ -29,46 +33,48 @@ const Home = async () => {
           
         </div>
 
+
+        <div className="flex gap-3 mt-6 overflow-x-scroll">
+          {quickSearchOptions.map((option) => (
+            <Button className="gap-2" variant="secondary" key={option.title}>
+              <Image src={option.imageUrl}  width={16} height={16} alt={option.title} />
+              {option.title}
+            </Button>
+          ))}
+
+        </div>
+        
+
         <div className="relative w-full h-[150px] mt-6">
           <Image alt="Agende nos melhores" 
           src="/banner-01.png" fill className="object-cover rounded-xl"/>
         </div>
 
-        <h2 className="uppercase text-xs font-bold text-gray-400 mt-6 mb-2">Agendamentos</h2>
-
-        <Card>
-          <CardContent className="flex justify-between p-0">
-            <div className="flex flex-col gap-2 py-5 pl-5">
-              <Badge className="w-fit">
-                Confirmado
-              </Badge>
-              <h3 className="font-semibold">Corte de cabelo</h3>
-              <div className="flex items-center gap-2">
-                <Avatar className="h-6 w-6">
-                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
-                </Avatar>
-                <p className="text-sn">Vintage Barber</p>
-              </div>
-            </div>
-            <div className="flex flex-col items-center justify-center px-5 border-l-2 border-solid">
-              <p className="text-sm">Agosto</p>
-              <p className="text-xl">22</p>
-              <p className="text-sm">15:99</p>
-            </div>
-          </CardContent>
-        </Card>
+        <BookingItem />
 
         <h2 className="uppercase text-xs font-bold text-gray-400 mt-6 mb-2">Recomendados</h2>
 
         <div className="flex gap-4 overflow-auto">
           {barbershops.map(barbershop => <BarbershopItem key={barbershop.id} barbershop={barbershop} />)}
         </div>
+
+        <h2 className="uppercase text-xs font-bold text-gray-400 mt-6 mb-2">Popular</h2>
+
+        <div className="flex gap-4 overflow-auto">
+          {popularBarbershops.map(barbershop => <BarbershopItem key={barbershop.id} barbershop={barbershop} />)}
+        </div>
+
+        <footer>
+          <Card>
+          <CardContent className="py-6 px-5">
+            <p className="text-sm text-gray-400">
+              © 2023 Copyright <span className="font-bold">FSW Barber</span>
+            </p>
+          </CardContent>
+        </Card>
+        </footer>
+
       </div>
-
-      
-      
-      
-
     </div>
   );
 }
