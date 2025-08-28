a8a6b4492af659254a65502c4f5c43dd726d302f
Jhonesm13 <jhone.menezes@edu.pucrs.br>
2025-08-28 00:58:08 -0300

    initial setup for google auth

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -1,10 +1,11 @@
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react";
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react";
 import { Button } from "./ui/button";
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet";
 import { Avatar, AvatarImage } from "./ui/avatar";
 import Link from "next/link";
 import { quickSearchOptions } from "../_constants/search";
 import Image from "next/image";
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "./ui/dialog";
 
 const SidebarSheet = () => {
     return ( 
@@ -13,15 +14,38 @@ const SidebarSheet = () => {
                 <SheetTitle className="text-left">Menu</SheetTitle>
             </SheetHeader>
 
-            <div className="py-5 border-b border-solid flex items-center gap-3">
-                <Avatar>
+            <div className="py-5 border-b border-solid flex items-center gap-3 justify-between">
+                <h2 className="font-bold">Olá, faça o seu login!</h2>
+                <Dialog>
+                    <DialogTrigger asChild>
+                        <Button size="icon">
+                            <LogInIcon />
+                        </Button>
+                    </DialogTrigger>
+
+                    <DialogContent className="w-[90%]">
+                        <DialogHeader>
+                            <DialogTitle>Faça login na plataforma</DialogTitle>
+                            <DialogDescription>
+                                Conecte-se usando sua conta do google.
+                            </DialogDescription>
+                        </DialogHeader>
+
+                        <Button variant="outline" className="gap-1 font-bold">
+                            <Image alt="Fazer login com o Google" src="/google.svg" width={18} height={18} />
+                            Google
+                        </Button>
+                    </DialogContent>
+                </Dialog>
+                
+                {/* <Avatar>
                     <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
                 </Avatar>
 
                 <div>
                     <p className="font-bold">Jhone Salvador</p>
                     <p className="text-xs">jhone.menezes@edu.pucrs.br</p>
-                </div>
+                </div> */}
             </div>
 
             <div className="py-5 flex flex-col gap-2 border-b border-solid">
